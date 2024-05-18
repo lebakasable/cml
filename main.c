aeb495568d4feb5451755943066a935020f1ab30
LeBakaSable <hugogole.ito@gmail.com>
2024-05-18 17:26:47 +0200

    feat: summer (https://en.wikipedia.org/wiki/Adder_(electronics))

--- main.c
+++ main.c
@@ -3,77 +3,71 @@
 #define ML_IMPLEMENTATION
 #include "ml.h"
 
-// https://en.wikipedia.org/wiki/Adder_(electronics)
-float td_sum[] = {
-  0, 0, 0, 0, 0, 0,
-  0, 0, 0, 1, 0, 1,
-  0, 1, 0, 1, 0, 1,
-  0, 1, 1, 0, 1, 1,
-};
-
-// https://en.wikipedia.org/wiki/XOR_gate
-float td_xor[] = {
-  0, 0, 0,
-  0, 1, 1,
-  1, 0, 1,
-  1, 1, 0,
-};
-
-// https://en.wikipedia.org/wiki/OR_gate
-float td_or[] = {
-  0, 0, 0,
-  0, 1, 1,
-  1, 0, 1,
-  1, 1, 1,
-};
+#define BITS 2
 
 int main(void)
 {
   srand(time(NULL));
 
-  float *td = td_xor;
+  size_t n = 1<<BITS;
+  size_t rows = n*n;
+  Mat ti = mat_alloc(rows, 2*BITS);
+  Mat to = mat_alloc(rows, BITS + 1);
+  for (size_t i = 0; i < ti.rows; ++i) {
+    size_t x = i/n;
+    size_t y = i%n;
+    size_t z = x + y;
+    for (size_t j = 0; j < BITS; ++j) {
+      MAT_AT(ti, i, j) = (x>>j)&1;
+      MAT_AT(ti, i, j + BITS) = (y>>j)&1;
+      MAT_AT(to, i, j) = (z>>j)&1;
+    }
+    MAT_AT(to, i, BITS) = z >= n;
+  }
 
-  size_t stride = 3;
-  size_t n = 4;
-
-  Mat ti = {
-    .rows = n,
-    .cols = 2,
-    .stride = stride,
-    .es = td,
-  };
-
-  Mat to = {
-    .rows = n,
-    .cols = 1,
-    .stride = stride,
-    .es = td + 2,
-  };
-
-  size_t desc[] = {2, 2, 1};
-  Model m = model_alloc(desc, ML_ARRAY_LEN(desc));
-  Model g = model_alloc(desc, ML_ARRAY_LEN(desc));
+	size_t arch[] = {2*BITS, 2*BITS + 1, BITS + 1};
+  Model m = model_alloc(arch, ML_ARRAY_LEN(arch));
+  Model g = model_alloc(arch, ML_ARRAY_LEN(arch));
   model_rand(m, 0, 1);
 
   float rate = 1;
 
-  for (size_t i = 0; i < 100000; ++i) {
-#if 0
-    float eps = 1e-1;
-    model_fdiff(m, g, eps, ti, to);
-#else
+  for (size_t i = 0; i < 10*1000; ++i) {
     model_backprop(m, g, ti, to);
-#endif
-    model_learn(m, g, rate);
+  	model_learn(m, g, rate);
   }
 
-  for (size_t i = 0; i < 2; ++i) {
-    for (size_t j = 0; j < 2; ++j) {
-      MAT_AT(MODEL_IN(m), 0, 0) = i;
-      MAT_AT(MODEL_IN(m), 0, 1) = j;
+	size_t fails = 0;
+  for (size_t x = 0; x < n; ++x) {
+    for (size_t y = 0; y < n; ++y) {
+      size_t z = x + y;
+      for (size_t j = 0; j < BITS; ++j) {
+        MAT_AT(MODEL_IN(m), 0, j) = (x>>j)&1;
+        MAT_AT(MODEL_IN(m), 0, j + BITS) = (y>>j)&1;
+      }
       model_forward(m);
-      printf("%zu ^ %zu = %f\n", i, j, MAT_AT(MODEL_OUT(m), 0, 0));
+      if (MAT_AT(MODEL_OUT(m), 0, BITS) > 0.5f) {
+        if (z < n) {
+					printf("%zu + %zu - (OVERFLOW<>%zu)\n", x, y, z);
+					fails += 1;
+        }
+      } else {
+        size_t a = 0;
+        for (size_t j = 0; j < BITS; ++j) {
+  				size_t bit = MAT_AT(MODEL_OUT(m), 0, j) > 0.5f;
+  				a |= bit<<j;
+        }
+        if (z != a) {
+					printf("%zu + %zu - (OVERFLOW|%zu<>%zu)\n", x, y, z, a);
+					fails += 1;
+        }
+        printf("%zu\n", a);
+      }
     }
+  }
+
+  if (fails == 0) {
+    printf("OK\n");
   }
 
   return 0;
